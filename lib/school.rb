385cabff042b0a755a58f5518cbfa295f3809dfb
Olivia Auzenne <oauzenne@gmail.com>
2019-02-22 16:36:09 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,25 @@
-# code here!
+class School
+attr_accessor :student_name, :grade_level
+attr_reader :school_name :roster
+
+  def initialize(school_name)
+    @school_name=school_name
+    @roster={}
+  end
+  
+  def roster(student_name,grade_level)
+    @roster=roster
+    roster = {#{student_name} => #{grade_level}}
+    ROSTER_LIST<<roster
+  end
+  
+  def add_student(student_name, grade_level)
+end
+  
+  
+  
+  
+  
+  
+end
+  
